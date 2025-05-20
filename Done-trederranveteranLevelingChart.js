810ac4c0832286a0190fc2f4451053c09d024b7c
atarunox <ffxi.nox@hotmail.com>
2025-05-19 19:32:51 -0700

    Update and rename trederranveteranLevelingChart.js to Done-trederranveteranLevelingChart.js

---
+++ Done-trederranveteranLevelingChart.js
@@ -0,0 +1,21 @@
+const trederranVeteranLevelingChart = {
+  2: {
+    name: "Dark Stone Hunter",
+    description: "Any time you gain 1 or more Dark Stone, you may gain 1 extra."
+  },
+  3: { bonus: "+1 Corruption Resistance" },
+  4: { bonus: "+1 Move" },
+  5: { bonus: "+1 Strength or +1 Cunning", extra: "+D6 Sanity" },
+  6: { bonus: "+1 Lore or +1 Spirit", extra: "+D6 Sanity" },
+  7: { bonus: "+3 Health and +3 Sanity" },
+  8: { bonus: "+1 Agility or +1 Spirit", extra: "+D6 Health" },
+  9: { bonus: "+1 Initiative" },
+  10: { bonus: "+1 Max Grit" },
+  11: { bonus: "+2 Side Bag Token Capacity" },
+  12: {
+    name: "Personalized Gear",
+    description: "Choose any Item – you have to gain an extra Upgrade Slot (Limit once per Item)."
+  }
+};
+
+export default trederranVeteranLevelingChart;
