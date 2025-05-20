c48931524106b5c4b01cb4335fe68f7acce0729d
atarunox <ffxi.nox@hotmail.com>
2025-05-19 19:35:09 -0700

    Update and rename skillTree_DarkStoneShaman.js to Done-skillTree_DarkStoneShaman.js

---
+++ Done-skillTree_DarkStoneShaman.js
@@ -0,0 +1,28 @@
+const darkStoneShamanUpgradeChart = [
+  [
+    { name: "War Shaman", description: "New Spirit Magik Battle or Protection Spell." },
+    { name: "Call of the Wild", description: "New Spirit Magik Shapeshifting Spell." },
+    { name: "Harmony", description: "You no longer need to roll for Corruption from Dark Stone at the end of each Adventure.", bonus: "+1 Max Grit" },
+    { name: "Storytelling", description: "", bonus: "+1 Lore and +1 Spirit Magik" }
+  ],
+  [
+    { name: "Spirit Sacrifice", description: "Once per turn, when you kill an Enemy, you may use 2 Magik to Recover a Grit.", bonus: "+1 Agility" },
+    { name: "Tribal Dance", description: "While in an Animal Form, you may Re-roll 1 Defense roll or 1 To Hit roll per turn.", bonus: "+1 Initiative" },
+    { name: "Attuned", description: "Dark Stone used to enhance your Spells now add 3 extra casting dice each instead." },
+    { name: "Wisdom of Ages", description: "New Spirit Magik Battle or Protection or Shapeshifting Spell.", bonus: "+1 Cunning" }
+  ],
+  [
+    { name: "Battle Chant", description: "New Spirit Magik Battle or Protection Spell.", bonus: "+1 Spirit Magik" },
+    { name: "Animal Nature", description: "New Spirit Magik Shapeshifting Spell.", bonus: "+1 Spirit Magik" },
+    { name: "Void Strength", description: "You are +1 Health for each unique Item you carry that has a Dark Stone icon on it.", bonus: "+1 Strength" },
+    { name: "Ancestral Guide", description: "Once per turn, you may use 2 Dark Stone to prevent the Darkness from moving on the Depth Track on the D6 roll of 3+." }
+  ],
+  [
+    { name: "Spirit Champion", description: "", bonus: "+1 Max Grit and Defense 3+" },
+    { name: "One with the Spirits", description: "You may now cast other Spirit Magik, even while in Animal Form.", bonus: "+1 Combat" },
+    { name: "Light as a Feather", description: "Items you carry that have a Dark Stone icon count as having 1 less Weight.", bonus: "+1 Move" },
+    { name: "Tribal Elder", description: "Once per turn, you may add an extra Power Level to a single Spell you are casting.", bonus: "+1 Spirit Magik" }
+  ]
+];
+
+export default darkStoneShamanUpgradeChart;
